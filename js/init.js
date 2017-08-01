a5d06d7411ae51ff081432a9f7e46322c48b7d6f
RodrigoKM <rodrigokoythi@hotmail.com>
2017-08-01 02:58:08 -0300

    update styles

--- js/init.js
+++ js/init.js
@@ -1,8 +1,34 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 80
+    });
 
+    var options = [
+      {
+        selector: '#image-test', offset: 250, callback: function (el) {
+          fadeInTag('#image-test');
+        }
+      },
+      {
+        selector: '#palestrantes', offset: 250, callback: function (el) {
+          fadeInTag('#palestrantes');
+        }
+      },
+      {
+        selector: '#local', offset: 250, callback: function (el) {
+          fadeInTag('#local');
+        }
+      }
+    ];
+
+    function fadeInTag(id) {
+      $(id).animate({ 'opacity': 1 }, 1500);
+    }
+
+    Materialize.scrollFire(options);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
